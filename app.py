21ad2921a9a686ff18145313fa4cd1d33ad06cdb
arthur <arthurrabelo2015@hotmail.com>
2025-09-07 12:40:18 -0300

    new dre model and further features in the app

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 import streamlit as st
-import emoji
+import pandas as pd
 
 st.title("Análise de Demonstração de Resultados")
 st.text("📋 Gere a análise de sua DRE automaticamente, de maneira rápida, fácil e visual.")
 
-with open("dre_modelo.xlsx", "rb") as file:
+def read_sheet():
+    uploaded_file = st.session_state["dre_file"]
+    if uploaded_file is not None:
+        filename = uploaded_file.name.lower()
+
+        if filename.endswith(".csv"):
+            df = pd.read_csv(uploaded_file)
+            generate_dre_analysis(df)
+        elif filename.endswith(".xlsx"):
+            df = pd.read_excel(uploaded_file)
+            generate_dre_analysis(df)
+        else:
+            st.error("Unsupported file type")
+
+def generate_dre_analysis(df: pd.DataFrame):
+    year = round(df['Ano'].tolist()[0])
+    operational_profit = df.loc[df['Dados'] == "Lucro operacional", "Ano"].values
+    liquid_profit = df.loc[df['Dados'] == "Lucro líquido", "Ano"].values
+    total_sales = df.loc[df['Dados'] == "Total de Vendas", "Ano"].values
+    total_assets = df.loc[df['Dados'] == "Total de ativos", "Ano"].values
+    fix_assets = df.loc[df['Dados'] == "Ativos fixos", "Ano"].values
+
+
+with open("modelo_dre.xlsx", "rb") as file:
     st.download_button(
         label="Baixar o modelo de planilha",
         data=file,
@@ -13,3 +36,17 @@ with open("dre_modelo.xlsx", "rb") as file:
         icon="📥",
         help="Este deve ser o modelo anexado para realização dos cálculos. Caso adicione outro modelo, os cálculos não funcionarão corretamente."
     )
+
+st.text("Após baixar o modelo acima e preenchê-lo, anexe-o abaixo 👇")
+
+st.file_uploader(
+    label="Anexe aqui sua planilha preenchida",
+    type= ["csv", "xlsx"],
+    key="dre_file",
+    label_visibility="hidden",
+    on_change=read_sheet
+)
+
+uploaded_file = st.session_state["dre_file"]
+if uploaded_file is not None:
+    st.info("📊 Planilha carregada com sucesso! A análise de sua DRE está sendo gerada...")
